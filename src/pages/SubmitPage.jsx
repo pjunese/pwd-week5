85ed7fcb4ae6c4c84412f49fbbe50de372112453
pjunese <pjunese99@gmail.com>
2025-10-11 19:31:42 +0900

    delete front

--- src/pages/SubmitPage.jsx
+++
@@ -1,9 +0,0 @@
-/* src/pages/SubmitPage.jsx */
-import React from 'react';
-import SubmitRestaurant from '../components/SubmitRestaurant';
-
-function SubmitPage() {
-  return <SubmitRestaurant />;
-}
-
-export default SubmitPage;
